5e8ed175446d8ce096464bb7007b5edac65b5380
katamatata <51786805+katamatata@users.noreply.github.com>
2019-12-04 15:15:37 +0100

    changing styling colors, resizing logo, adding cooking instructions

--- javascript/main.js
+++ javascript/main.js
@@ -53,7 +53,7 @@ function showResults(recipeList) {
     for (let i = 0; i < recipeList.length; i++) {
         let recipeEl = document.createElement('div');
         allElements.push(recipeEl);
-        recipeEl.className = 'recipeEl';
+        recipeEl.className = 'recipeListEl';
         recipeEl.onclick = function () {
             showRecipe(recipeList[i].id);
 
@@ -63,7 +63,7 @@ function showResults(recipeList) {
 
             recipeEl.classList.add('selected');
         }
-        document.getElementById("recipeList").appendChild(recipeEl);
+        document.getElementById("recipeListSection").appendChild(recipeEl);
 
         let thumbnailImg = document.createElement('img');
         thumbnailImg.className = 'thumb';
@@ -71,18 +71,18 @@ function showResults(recipeList) {
         recipeEl.appendChild(thumbnailImg);
 
         let recipeInfoEl = document.createElement('div');
-        recipeInfoEl.className = 'recipeInfo';
+        recipeInfoEl.className = 'recipeListInfo';
         recipeEl.appendChild(recipeInfoEl);
 
         let recipeName = document.createElement('h2');
-        recipeName.className = 'recipeName';
+        recipeName.className = 'recipeListName';
         recipeName.textContent = recipeList[i].title;
         recipeInfoEl.appendChild(recipeName);
     }
 }
 
 function clearResults() {
-    let clearResults = document.getElementById("recipeList");
+    let clearResults = document.getElementById("recipeListSection");
     clearResults.innerHTML = "";
 }
 
@@ -103,20 +103,27 @@ async function showRecipe(id) {
 
     let bigImg = document.createElement('img');
     bigImg.src = recipeFullInfo.image;
-    document.getElementById("recipe").appendChild(bigImg);
+    document.getElementById("recipeSection").appendChild(bigImg);
+
 
     let recipeInfoEl = document.createElement('div');
     recipeInfoEl.className = 'recipeInfo';
-    document.getElementById("recipe").appendChild(recipeInfoEl);
+    document.getElementById("recipeSection").appendChild(recipeInfoEl);
+
 
     let recipeName = document.createElement('h2');
     recipeName.className = 'recipeName';
     recipeName.textContent = recipeFullInfo.title;
     recipeInfoEl.appendChild(recipeName);
 
+
+    let iconsWrapper = document.createElement('div');
+    iconsWrapper.className = 'iconsWrapper';
+    recipeInfoEl.appendChild(iconsWrapper);
+
     let servingsEl = document.createElement('div');
     servingsEl.className = 'servingsEl';
-    recipeInfoEl.appendChild(servingsEl);
+    iconsWrapper.appendChild(servingsEl);
 
     let utensilsIcon = document.createElement('i');
     utensilsIcon.className = 'fas fa-utensils';
@@ -131,9 +138,10 @@ async function showRecipe(id) {
     }
     servingsEl.appendChild(servings);
 
+
     let cookingTimeEl = document.createElement('div');
     cookingTimeEl.className = 'cookingTimeEl';
-    recipeInfoEl.appendChild(cookingTimeEl);
+    iconsWrapper.appendChild(cookingTimeEl);
 
     let clockIcon = document.createElement('i');
     clockIcon.className = 'far fa-clock';
@@ -141,28 +149,60 @@ async function showRecipe(id) {
 
     let cookingTime = document.createElement('span');
     cookingTime.className = 'cookingTime';
-    cookingTime.textContent = recipeFullInfo.readyInMinutes + ' Min';
+    cookingTime.textContent = recipeFullInfo.readyInMinutes + ' min';
     cookingTimeEl.appendChild(cookingTime);
+
+
+    let heartEl = document.createElement('div');
+    heartEl.className = 'heartEl';
+    iconsWrapper.appendChild(heartEl);
 
     let heartIcon = document.createElement('i');
     heartIcon.className = 'fas fa-heart';
-    
+    heartEl.appendChild(heartIcon);
+
+
+    let infoWrapper = document.createElement('div');
+    infoWrapper.className = 'infoWrapper';
+    recipeInfoEl.appendChild(infoWrapper);
+
+    let ingredientsWrapper = document.createElement('div');
+    ingredientsWrapper.className = 'ingredientsWrapper';
+    infoWrapper.appendChild(ingredientsWrapper);
+
+    let ingredientsWrapperName = document.createElement('p');
+    ingredientsWrapperName.className = 'ingredientsWrapperName';
+    ingredientsWrapperName.textContent = 'Ingredients:';
+    ingredientsWrapper.appendChild(ingredientsWrapperName);
 
     let ingredientsEl = document.createElement('ul');
     ingredientsEl.className = 'ingredientsEl';
-    ingredientsEl.textContent = 'Ingredients: ';
-    recipeInfoEl.appendChild(ingredientsEl);
+    ingredientsWrapper.appendChild(ingredientsEl);
 
-    for (let i=0; i < recipeFullInfo.extendedIngredients.length; i++) {
+    for (let i = 0; i < recipeFullInfo.extendedIngredients.length; i++) {
         let ingredient = document.createElement('li');
         ingredient.textContent = recipeFullInfo.extendedIngredients[i].original;
         // console.log(ingredient);
         ingredientsEl.appendChild(ingredient);
     }
+
+    let instructionsWrapper = document.createElement('div');
+    instructionsWrapper.className = 'instructionsWrapper';
+    infoWrapper.appendChild(instructionsWrapper);
+
+    let instructionsWrapperName = document.createElement('p');
+    instructionsWrapperName.className = 'instructionsWrapperName';
+    instructionsWrapperName.textContent = 'Preparation:';
+    instructionsWrapper.appendChild(instructionsWrapperName);
+
+    let instructionsEl = document.createElement('p');
+    instructionsEl.className = 'instructionsEl';
+    instructionsEl.textContent = recipeFullInfo.instructions;
+    instructionsWrapper.appendChild(instructionsEl);
 }
 
 function clearRecipe() {
-    let clearResults = document.getElementById("recipe");
+    let clearResults = document.getElementById("recipeSection");
     clearResults.innerHTML = "";
     // console.log(clearResults);
 }
